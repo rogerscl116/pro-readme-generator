e4097402b07301609922fdf90d3052b2f9301990
charity.rogers <rogerscl116@gmail.com>
2021-06-09 22:10:47 -0500

    start on array of questions for user input

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require("util");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubUsername => {
+            if (githubUsername) {
+                return true;
+            } else {
+                return console.log("Please enter your GitHub username!")
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubRepo',
+        message: 'What is the name of your GitHub repo? (Required)',
+        validate: githubRepo => {
+            if (githubRepo) {
+                return true;
+            } else {
+                return console.log("Please enter your GitHub repo!")
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project? (Required)',
+        validate: projectTitleInput => {
+            if (projectTitleInput) {
+                return true;
+            } else {
+                return console.log("Please enter a project title!")
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'Provide a description of your project. (Required)',
+        validate: projectDescription => {
+            if (projectDescription) {
+                return true;
+            } else {
+                return console.log("Please enter a project description!")
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'projectSteps',
+        message: 'If applicable, provide the steps required to install your project.',   
+    },
+
+    
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
